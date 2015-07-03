49ae3f0c1fe1ba7753baee7644a389c4c30ba7cd
ami-GS <1991.daiki@gmail.com>
2015-07-03 23:15:42 +0900

    add frame header

--- frame.go
+++ frame.go
@@ -15,3 +15,44 @@ const (
 	AckFrameType                = 0x40
 	CongestionFeedbackFrameType = 0x20
 )
+
+// Frame Header
+/*
++--------+--------+--------+--------+--------+---    ---+
+ Public |    Connection ID (0, 8, 32, or 64)    ...    | ->
+|Flags(8)|      (variable length)                       |
++--------+--------+--------+--------+--------+---    ---+
+
+    9       10       11        12
++--------+--------+--------+--------+
+|      Quic Version (32)            | ->
+|         (optional)                |
++--------+--------+--------+--------+
+
+    13      14       15        16        17       18       19       20
++--------+--------+--------+--------+--------+--------+--------+--------+
+|         Sequence Number (8, 16, 32, or 48)          |Private | FEC (8)|
+|                         (variable length)           |Flags(8)|  (opt) |
++--------+--------+--------+--------+--------+--------+--------+--------+
+*/
+
+type FrameHeader struct {
+	PublicFlags    byte
+	ConnectionID   uint64
+	Version        uint32
+	SequenceNumber uint64
+	PrivateFlags   byte
+	FEC            byte
+}
+
+func NewFrameHeader(publicFlags byte, connectionID uint64, version uint32, sequenceNumber uint64, privateFlags, fec byte) *FrameHeader {
+	fh := &FrameHeader{
+		publicFlags,
+		connectionID,
+		version,
+		sequenceNumber,
+		privateFlags,
+		fec,
+	}
+	return fh
+}
