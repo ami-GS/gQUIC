23bc40a52d2fab5c3e6085b68ed8099a8b9e141c
ami-GS <1991.daiki@gmail.com>
2016-03-13 23:47:03 +0900

    implement inside of Send

--- transport.go
+++ transport.go
@@ -13,8 +13,19 @@ func (self *Transport) Connect() (err error) {
 	return nil
 }
 
-func (self *Transport) Send() (err error) {
-	return nil
+func (self *Transport) Send(p Packet) (err error) {
+	wire, err := p.GetWire()
+	if err != nil {
+		return err
+	}
+	id := p.GetConnectionID()
+	conn, ok := self.Conns[id]
+	if ok {
+		_, err = conn.Write(wire)
+	} else {
+		return CONNECTION_NOT_FOUND
+	}
+	return err
 }
 
 func (self *Transport) Recv() (err error) {
