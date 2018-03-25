113ccb8932cf6c86757fd159c0264de5ed2e47ec
ami-GS <1991.daiki@gmail.com>
2018-03-25 17:54:54 +0900

    implement loop for waiting client's connection

--- server.go
+++ server.go
@@ -1,8 +1,62 @@
 package quic
 
+import (
+	"github.com/ami-GS/gQUIC/utils"
+)
+
 type Server struct {
-	St      *Transport
-	clients []*Client
+	St              *Transport
+	Clients         map[uint64]*Client
+	SupportVersions []uint32
+}
+
+func NewServer() (*Server, error) {
+	st, err := NewTransport("cert", "key")
+	if err != nil {
+		return nil, err
+	}
+	return &Server{
+		St:      st,
+		Clients: make(map[uint64]*Client),
+	}, nil
+}
+
+func (self *Server) ListenAndServe(addPair string) error {
+	udpAddr, err := utils.ParseAddressPair(addPair)
+	if err != nil {
+		return err
+	}
+
+	err = self.St.Listen(udpAddr)
+	if err != nil {
+		return err
+	}
+
+	for {
+		p, _, rAddr, err := self.St.Recv()
+		if err != nil {
+			break
+		}
+		client, ok := self.Clients[p.GetConnectionID()]
+		if ok {
+			client.RecvChan <- p
+		} else {
+			client, err = NewClient(true) // true stands for server side object
+			if err != nil {
+				break
+			}
+			client.Conn, err = NewConnection(rAddr)
+			client.Conn.ConnectionID = p.GetConnectionID()
+			if err != nil {
+				break
+			}
+			self.Clients[p.GetConnectionID()] = client
+			go client.ReadLoop()
+			client.RecvChan <- p
+		}
+	}
+
+	return err
 }
 
 func (self *Server) FramePacket(frames []*Frame) {}
