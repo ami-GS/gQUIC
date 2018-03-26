80c82a2f0df2761327526c1088c4dcaa409300c9
ami-GS <1991.daiki@gmail.com>
2018-03-26 19:06:52 +0900

    implement stream state change, and String

--- stream.go
+++ stream.go
@@ -1,5 +1,7 @@
 package quic
 
+import "fmt"
+
 type State byte
 
 const (
@@ -8,19 +10,29 @@ const (
 	CLOSED
 )
 
+func (s State) String() string {
+	return []string{
+		"OPEN",
+		"HALF_CLOSED",
+		"CLOSED",
+	}[s]
+}
+
 type Stream struct {
 	*Conn
-	ID     uint32
-	State  State
-	Window *Window
+	ID        uint32
+	State     State
+	PeerState State
+	Window    *Window
 }
 
 func NewStream(streamID uint32, conn *Conn) (stream *Stream) {
 	stream = &Stream{
-		Conn:   conn,
-		ID:     streamID,
-		State:  OPEN,
-		Window: NewWindow(),
+		Conn:      conn,
+		ID:        streamID,
+		State:     OPEN,
+		PeerState: OPEN,
+		Window:    NewWindow(),
 	}
 	return
 }
@@ -35,16 +47,12 @@ func ReadStreamLevelFrame(conn *Conn, f StreamLevelFrame) error {
 			// implecitely created
 			stream = conn.GenStream(id)
 		}
-		if frame.Fin == true {
-			// Normal termination
-		}
 		stream.ApplyStreamFrame(frame)
 	case *WindowUpdateFrame:
 		if !ok {
 			return QUIC_PACKET_FOR_NONEXISTENT_STREAM
 		}
 		stream.ApplyWindowUpdateFrame(frame)
-
 	case *BlockedFrame:
 		if !ok {
 			return QUIC_PACKET_FOR_NONEXISTENT_STREAM
@@ -61,7 +69,16 @@ func ReadStreamLevelFrame(conn *Conn, f StreamLevelFrame) error {
 }
 
 func (self *Stream) ApplyStreamFrame(f *StreamFrame) {
-
+	if f.Fin {
+		self.PeerState = HALF_CLOSED
+		if self.State == HALF_CLOSED {
+			self.State = CLOSED
+			self.PeerState = CLOSED
+		}
+	}
+	if self.PeerState == HALF_CLOSED || self.PeerState == CLOSED {
+		// TODO : emit error
+	}
 }
 
 func (self *Stream) ApplyBlockedFrame(f *BlockedFrame) {
@@ -74,4 +91,24 @@ func (self *Stream) ApplyWindowUpdateFrame(f *WindowUpdateFrame) {
 
 func (self *Stream) ApplyRstStream(f *RstStreamFrame) {
 
+}
+
+func (self *Stream) SendStreamFrame(f *StreamFrame) {
+	if self.State == HALF_CLOSED || self.State == CLOSED {
+		// TODO : emit error
+		// cannot send
+	}
+	if f.Fin {
+		self.State = HALF_CLOSED
+		if self.PeerState == HALF_CLOSED {
+			self.State = CLOSED
+			self.PeerState = CLOSED
+		}
+	}
+}
+
+func (self *Stream) String() string {
+	str := fmt.Sprintf("Stream ID:%d\n\tLocal State: %s\n\tPeer  State: %s",
+		self.ID, self.State.String(), self.PeerState.String())
+	return str
 }
