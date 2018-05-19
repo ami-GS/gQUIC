cabba85b8abd2b8d63cd71931b021d1577747b84
ami-GS <1991.daiki@gmail.com>
2018-05-19 15:56:42 +0900

    implement rough state machine for send/receive stream

--- latest/stream.go
+++ latest/stream.go
@@ -24,77 +24,75 @@ func (s *StreamManager) GetOrNewRecvStream(streamID qtype.StreamID, sess *Sessio
 	return st, nil
 }
 
-func (s *StreamManager) GetOrNewSendStream(streamID qtype.StreamID, sess *Session) (stream Stream, err error) {
+func (s *StreamManager) GetOrNewSendStream(streamID qtype.StreamID, sess *Session) (*SendStream, error) {
 	sidVal := streamID.GetValue()
 
 	stream, ok := s.streamMap[sidVal]
 	if ok {
-		if _, ok := stream.(SendStream); !ok {
+		st, ok := stream.(SendStream)
+		if !ok {
 			// TODO: what error?
-			return nil, err
+			return nil, nil
 		}
-		return stream, nil
+		return &st, nil
 	}
-	stream = newSendStream(streamID, sess)
-	s.streamMap[sidVal] = stream
-	return stream, nil
+	st := newSendStream(streamID, sess)
+	s.streamMap[sidVal] = st
+	return st, nil
 }
 
-func (s *StreamManager) GetOrNewSendRecvStream(streamID qtype.StreamID, sess *Session) (stream Stream, err error) {
+func (s *StreamManager) GetOrNewSendRecvStream(streamID qtype.StreamID, sess *Session) (*SendRecvStream, error) {
 	sidVal := streamID.GetValue()
 
 	stream, ok := s.streamMap[sidVal]
 	if ok {
-		if _, ok := stream.(SendRecvStream); !ok {
+		st, ok := stream.(SendRecvStream)
+		if !ok {
 			// TODO: what error?
-			return nil, err
+			return nil, nil
 		}
-		return stream, nil
+		return &st, nil
 	}
-	stream = newSendRecvStream(streamID, sess)
-	s.streamMap[sidVal] = stream
-	return stream, nil
+	st := newSendRecvStream(streamID, sess)
+	s.streamMap[sidVal] = st
+	return st, nil
 }
 func (s *StreamManager) handleStreamFrame(frame *StreamFrame) error {
 	stream, err := s.GetOrNewRecvStream(frame.StreamID, s.sess)
 	if err != nil {
 		return err
 	}
-	stream.handleStreamFrame(frame)
-	return nil
+	return stream.handleStreamFrame(frame)
 }
 func (s *StreamManager) handleStreamBlockedFrame(frame *StreamBlockedFrame) error {
 	stream, err := s.GetOrNewRecvStream(frame.StreamID, s.sess)
 	if err != nil {
 		return err
 	}
-	stream.handleStreamBlockedFrame(frame)
-	return nil
+	return stream.handleStreamBlockedFrame(frame)
 }
 func (s *StreamManager) handleRstStreamFrame(frame *RstStreamFrame) error {
 	stream, err := s.GetOrNewRecvStream(frame.StreamID, s.sess)
 	if err != nil {
 		return err
 	}
-	stream.handleRstStreamFrame(frame)
-	return nil
+	return stream.handleRstStreamFrame(frame)
+
 }
 func (s *StreamManager) handleStopSendingFrame(frame *StopSendingFrame) error {
-	// TODO: This MUST not create New Stream
-	stream, err := s.GetOrNewRecvStream(frame.StreamID, s.sess)
+	// TODO: This MUST not crenate New Stream
+	stream, err := s.GetOrNewSendStream(frame.StreamID, s.sess)
 	if err != nil {
 		return err
 	}
-	stream.handleStopSendingFrame(frame)
-	return nil
+	return stream.handleStopSendingFrame(frame)
 }
 func (s *StreamManager) handleMaxStreamDataFrame(frame *MaxStreamDataFrame) error {
-	stream, err := s.GetOrNewRecvStream(frame.StreamID, s.sess)
+	stream, err := s.GetOrNewSendStream(frame.StreamID, s.sess)
 	if err != nil {
 		return err
 	}
-	stream.handleMaxStreamDataFrame(frame)
-	return nil
+	return stream.handleMaxStreamDataFrame(frame)
 }
 
 type Stream interface {
@@ -118,10 +116,11 @@ func (s BaseStream) GetID() qtype.StreamID {
 
 type SendStream struct {
 	*BaseStream
-	//Window    *Window
+	// application data can be buffered at "Ready" state
+	SendBuffer []byte
 }
 
-func newSendStream(streamID qtype.StreamID, sess *Session) Stream {
+func newSendStream(streamID qtype.StreamID, sess *Session) *SendStream {
 	return &SendStream{
 		BaseStream: &BaseStream{
 			ID:    streamID,
@@ -134,32 +133,93 @@ func newSendStream(streamID qtype.StreamID, sess *Session) Stream {
 	}
 }
 
-func (s *SendStream) sendRstStreamFrame() {
-	s.State = qtype.StreamResetSent
-	//TODO: ResetRecvd after Ack
-}
-
-func (s *SendStream) sendFrame(f Frame) {
+func (s *SendStream) sendFrame(f Frame) (err error) {
+	if s.State == qtype.StreamDataRecvd || s.State == qtype.StreamResetRecvd {
+		// MUST NOT send any frame in the states above
+		return nil
+	}
 	switch frame := f.(type) {
 	case StreamFrame:
-		if s.State == qtype.StreamReady {
-			s.State = qtype.StreamSend
-		} else if s.State == qtype.StreamSend && frame.Finish {
-			s.State = qtype.StreamDataSent
+		if s.State == qtype.StreamResetSent {
+			// MUST NOT send any frame in the states above
+			return nil
 		}
+		err = s.sendStreamFrame(&frame)
 	case StreamBlockedFrame:
-		if s.State == qtype.StreamReady {
-			s.State = qtype.StreamSend
+		if s.State == qtype.StreamResetSent {
+			// MUST NOT send any frame in the states above
+			return nil
 		}
+		err = s.sendStreamBlockedFrame(&frame)
+	case RstStreamFrame:
+		err = s.sendRstStreamFrame(&frame)
 	default:
-		//error
+		// TODO: error
+		return nil
 	}
-	//s.sess.SendPacket(packet Packet)
+	return err
+}
+
+func (s *SendStream) sendStreamFrame(f *StreamFrame) error {
+	if s.State == qtype.StreamReady {
+		s.State = qtype.StreamSend
+	} else if s.State == qtype.StreamSend && f.Finish {
+		s.State = qtype.StreamDataSent
+	}
+	return nil
+}
+
+func (s *SendStream) sendStreamBlockedFrame(f *StreamBlockedFrame) error {
+	if s.State == qtype.StreamReady {
+		s.State = qtype.StreamSend
+	}
+	return nil
+}
+
+func (s *SendStream) sendRstStreamFrame(f *RstStreamFrame) error {
+	s.State = qtype.StreamResetSent
+	//TODO: ResetRecvd after Ack
+	return nil
+}
+
+// SendStream handle MaxStreamDataFrame  for flow control
+func (s *SendStream) handleMaxStreamDataFrame(f *MaxStreamDataFrame) error {
+	if s.State == qtype.StreamDataSent {
+		// ignore after being "Sent" state
+		return nil
+	}
+	return nil
+}
+
+// SendStream handle StopSending for receiving abondon request
+func (s *SendStream) handleStopSendingFrame(f *StopSendingFrame) error {
+	if s.State == qtype.StreamReady {
+		// TODO: connection error of type PROTOCOL_VIOLATION
+		return nil
+	}
+	// respond by RstStreamFrame
+	//s.sendRstStreamFrame(NewRstStreamFrame(f.StreamID, 0, 0))
+	return nil
+}
+
+// AckFrame comes via connection level handling
+func (s *SendStream) handleAckFrame(f *AckFrame) error {
+	if s.State == qtype.StreamDataSent {
+		// TODO: process all stream data are acked then,
+		s.State = qtype.StreamDataRecvd
+	} else if s.State == qtype.StreamResetSent {
+		s.State = qtype.StreamResetRecvd
+	}
+	return nil
+}
+
+func (s *SendStream) ackedAllStreamData() {
+	s.State = qtype.StreamDataRecvd
 }
 
 type RecvStream struct {
 	*BaseStream
-	//Window    *Window
+	ReorderBuffer map[uint64][]byte
 }
 
 func newRecvStream(streamID qtype.StreamID, sess *Session) *RecvStream {
@@ -172,32 +232,78 @@ func newRecvStream(streamID qtype.StreamID, sess *Session) *RecvStream {
 	}
 }
 
-func (s *RecvStream) handleRstStreamFrame(f *RstStreamFrame) {
+// returns data, is_reset
+func (s *RecvStream) ReadData() ([]byte, bool) {
+	// TODO: blocked until all data received and reordered?
+	//       should be implemented by two channel (dataCh and RstCh)
+	if s.State == qtype.StreamResetRecvd {
+		s.State = qtype.StreamResetRead
+		return nil, true
+	}
+	s.State = qtype.StreamDataRead
+	return nil, false
+}
+
+func (s *RecvStream) sendMaxStreamDataFrame(f *MaxStreamDataFrame) error {
+	//The receiver only sends MAX_STREAM_DATA in the "Recv" state
+	if s.State != qtype.StreamRecv {
+		return nil
+	}
+
+	return nil
+}
+
+func (s *RecvStream) sendStopSendingFrame(f *StopSendingFrame) error {
+	// A receiver can send STOP_SENDING in any state where it has not received a
+	// RST_STREAM frame; that is states other than "Reset Recvd" or "Reset Read"
+	if s.State == qtype.StreamResetRecvd || s.State == qtype.StreamResetRead {
+		return nil
+	}
+
+	return nil
+}
+
+func (s *RecvStream) handleRstStreamFrame(f *RstStreamFrame) error {
 	if s.State == qtype.StreamDataRecvd {
-		// TODO: Optional
+		// Optional
+		s.State = qtype.StreamResetRecvd
 	} else if s.State == qtype.StreamRecv || s.State == qtype.StreamSizeKnown {
 		s.State = qtype.StreamResetRecvd
 	}
-	// TODO: ResetRead after app read RST
+	// TODO: discard data received?
+	return nil
 }
-func (s *RecvStream) handleStreamFrame(f *StreamFrame) {
+
+func (s *RecvStream) handleStreamFrame(f *StreamFrame) error {
+	if s.State == qtype.StreamDataRecvd {
+		// ignore after receiving all data
+		return nil
+	}
+
 	if f.Finish {
 		s.State = qtype.StreamSizeKnown
 	}
+	//s.ReorderBuffer f.Data
+
+	// do something
+	s.State = qtype.StreamDataRecvd
+	return nil
 }
-func (s *RecvStream) handleStreamBlockedFrame(f *StreamBlockedFrame) {}
-func (s *RecvStream) handleMaxStreamDataFrame(f *MaxStreamDataFrame) {}
-func (s *RecvStream) handleStopSendingFrame(f *StopSendingFrame) {
-	// send RstStream
-	//s.sess.SendPacket(packet Packet)
+
+func (s *RecvStream) handleStreamBlockedFrame(f *StreamBlockedFrame) error {
+	if s.State == qtype.StreamDataRecvd {
+		// ignore after receiving all data
+		return nil
+	}
+	return nil
 }
 
 type SendRecvStream struct {
 	*BaseStream
 }
 
-func newSendRecvStream(streamID qtype.StreamID, sess *Session) Stream {
-	return &RecvStream{
+func newSendRecvStream(streamID qtype.StreamID, sess *Session) *SendRecvStream {
+	return &SendRecvStream{
 		BaseStream: &BaseStream{
 			ID:    streamID,
 			State: qtype.StreamIdle,
@@ -206,6 +312,7 @@ func newSendRecvStream(streamID qtype.StreamID, sess *Session) Stream {
 	}
 }
 
-func (s *SendRecvStream) handleRstStreamFrame(f *RstStreamFrame) {
+func (s *SendRecvStream) handleRstStreamFrame(f *RstStreamFrame) error {
 	s.State = qtype.StreamClosed
+	return nil
 }
