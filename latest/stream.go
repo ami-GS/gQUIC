6b9227d32c8ed7726f790202cdc29f774423a570
ami-GS <1991.daiki@gmail.com>
2018-05-24 20:26:41 +0900

    change IsValidID logic

--- latest/stream.go
+++ latest/stream.go
@@ -8,29 +8,42 @@ import (
 )
 
 type StreamManager struct {
-	streamMap map[uint64]Stream
-	sess      *Session
+	streamMap       map[uint64]Stream
+	maxStreamIDUni  qtype.StreamID
+	maxStreamIDBidi qtype.StreamID
+	sess            *Session
 }
 
 func NewStreamManager(sess *Session) *StreamManager {
+	uniID, _ := qtype.NewStreamID(8)
+	bidiID, _ := qtype.NewStreamID(2)
 	return &StreamManager{
 		streamMap: make(map[uint64]Stream),
 		sess:      sess,
+
+		// may be set after handshake, or MAX_STREAM_ID frame
+		maxStreamIDUni:  uniID,
+		maxStreamIDBidi: bidiID,
 	}
 }
 
 func (s *StreamManager) IsValidID(streamID *qtype.StreamID) error {
-	if streamID.GetValue() <= s.maxStreamID.GetValue() {
-		return nil
+	sid := streamID.GetValue()
+	if sid&0x2 == 0x2 {
+		// unidirectional
+		if streamID.GetValue() > s.maxStreamIDUni.GetValue() {
+			return qtype.StreamIDError
+		}
+	} else {
+		// bidirectional
+		if sid > s.maxStreamIDBidi.GetValue() {
+			return qtype.StreamIDError
+		}
 	}
-	return qtype.StreamIDError
+	return nil
 }
 
 func (s *StreamManager) GetOrNewRecvStream(streamID *qtype.StreamID, sess *Session) (*RecvStream, bool, error) {
-	err := s.IsValidID(streamID)
-	if err != nil {
-		return nil, false, err
-	}
 	sidVal := streamID.GetValue()
 	if sidVal&0x3 != 0x2 {
 		// error, uni directional should be 0x2 or 0x3
@@ -46,16 +59,17 @@ func (s *StreamManager) GetOrNewRecvStream(streamID *qtype.StreamID, sess *Sessi
 		}
 		return st, false, nil
 	}
+	// check whether ID is larger than MAX_STREAM_ID
+	err := s.IsValidID(streamID)
+	if err != nil {
+		return nil, false, err
+	}
 	st := newRecvStream(streamID, sess)
 	s.streamMap[sidVal] = st
 	return st, true, nil
 }
 
 func (s *StreamManager) GetOrNewSendStream(streamID *qtype.StreamID, sess *Session) (*SendStream, bool, error) {
-	err := s.IsValidID(streamID)
-	if err != nil {
-		return nil, false, err
-	}
 	sidVal := streamID.GetValue()
 	if sidVal&0x2 != 0x2 {
 		// error, uni directional should be 0x2 or 0x3
@@ -77,10 +91,6 @@ func (s *StreamManager) GetOrNewSendStream(streamID *qtype.StreamID, sess *Sessi
 }
 
 func (s *StreamManager) GetOrNewSendRecvStream(streamID *qtype.StreamID, sess *Session) (*SendRecvStream, bool, error) {
-	err := s.IsValidID(streamID)
-	if err != nil {
-		return nil, false, err
-	}
 	sidVal := streamID.GetValue()
 	if sidVal&0x2 == 0x2 {
 		// error, bi directional should be 0x0 or 0x1
