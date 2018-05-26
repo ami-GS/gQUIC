a0d1c4637954b64cb48ea4b28b1dcd688e63e6ef
ami-GS <1991.daiki@gmail.com>
2018-05-26 18:43:22 +0900

    implement assemble frame functionality

---
+++ latest/qtype/settings.go
@@ -0,0 +1,7 @@
+package qtype
+
+// MTU for IPv4 packet
+const MTUIPv4 = 1252
+
+// MTU for IPv6 packet
+const MTUIPv6 = 1232
--- latest/session.go
+++ latest/session.go
@@ -1,32 +1,87 @@
 package quiclatest
 
-import "github.com/ami-GS/gQUIC/latest/qtype"
+import (
+	"time"
+
+	"github.com/ami-GS/gQUIC/latest/qtype"
+)
 
 type Session struct {
 	// tls config
 	// connection
-	DestConnID      qtype.ConnectionID
-	SrcConnID       qtype.ConnectionID
-	DoneHandShake   bool
-	conn            *Connection
-	recvPacketChann chan Packet
-	streamManager   *StreamManager
+	DestConnID    qtype.ConnectionID
+	SrcConnID     qtype.ConnectionID
+	DoneHandShake bool
+	conn          *Connection
+	// from server/client to here
+	recvPacketChan chan Packet
+	// to fill sendFrameBuffer till timeout or filled by about MTU?
+	sendFrameChan chan Frame
+
+	streamManager *StreamManager
 
 	flowContoller *ConnectionFlowController
+
+	AssembleFrameChan chan struct{}
+	WaitFrameTimeout  *time.Ticker
 }
 
 func NewSession(conn *Connection, dstConnID, srcConnID qtype.ConnectionID) *Session {
 	sess := &Session{
-		DestConnID:      dstConnID,
-		SrcConnID:       srcConnID,
-		conn:            conn,
-		recvPacketChann: make(chan Packet),
-
+		DestConnID:     dstConnID,
+		SrcConnID:      srcConnID,
+		conn:           conn,
+		recvPacketChan: make(chan Packet),
+		// channel size should be configured or detect filled
+		sendFrameChan:     make(chan Frame, 100),
 		flowContoller:     &ConnectionFlowController{},
+		AssembleFrameChan: make(chan struct{}),
+		// TODO: this would be configurable
+		WaitFrameTimeout: time.NewTicker(10 * time.Millisecond),
 	}
 	sess.streamManager = NewStreamManager(sess)
 	return sess
 
+}
+
+func (s *Session) AssembleFrameLoop() {
+	assemble := func() []byte {
+		out := make([]byte, qtype.MTUIPv4)
+		byteSize := 0
+		for {
+			select {
+			case frame := <-s.sendFrameChan:
+				size := frame.GetWireSize()
+				if byteSize+size > qtype.MTUIPv4 {
+					// TODO: this should be problem
+					// big frame would never be sent
+					s.sendFrameChan <- frame
+					return out[:byteSize]
+				}
+				copy(out[byteSize:], frame.GetWire())
+				byteSize += size
+			default:
+				// If sendFrameChan is empty
+				return out[:byteSize]
+			}
+		}
+	}
+
+	var wire []byte
+	for {
+		select {
+		case <-s.AssembleFrameChan:
+			wire = assemble()
+		case <-s.WaitFrameTimeout.C:
+			wire = assemble()
+		}
+		if len(wire) == 0 {
+			continue
+		}
+		// 1. make Packet
+		// 2. send Packet
+	}
+	s.WaitFrameTimeout.Stop()
 }
 
 func (s *Session) SendPacket(packet Packet) error {
@@ -36,10 +91,11 @@ func (s *Session) SendPacket(packet Packet) error {
 	}
 	return s.conn.Write(wire)
 }
+
 func (s *Session) RecvPacketLoop() {
 	for {
 		select {
-		case p := <-s.recvPacketChann:
+		case p := <-s.recvPacketChan:
 			// TODO: parallel?
 			s.HandlePacket(p)
 		default:
