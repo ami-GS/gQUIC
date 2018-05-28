287655fc54270908e69e30de7d8d9e7eda95f682
ami-GS <1991.daiki@gmail.com>
2018-05-28 18:11:21 +0900

    implement more presisely

--- latest/flow_control.go
+++ latest/flow_control.go
@@ -1,5 +1,7 @@
 package quiclatest
 
+import "github.com/ami-GS/gQUIC/latest/qtype"
+
 type baseFlowController struct {
 	bytesSent       uint64
 	bytesReceived   uint64
@@ -15,31 +17,54 @@ type StreamFlowController struct {
 	connFC *ConnectionFlowController
 }
 
-func (s *StreamFlowController) SendableBySize(largestOffset uint64) bool {
-	return largestOffset <= s.MaxDataLimit
+func (s *StreamFlowController) SendableByOffset(offset uint64, fin bool) bool {
+	sendable := true
+	if !s.IsStreamZero && fin {
+		sendable = s.connFC.SendableByOffset(offset)
+	}
+	return sendable && offset <= s.MaxDataLimit
+}
+
+func (s *StreamFlowController) ReceivableByOffset(offset uint64, fin bool) error {
+	if !s.IsStreamZero && fin {
+		err := s.connFC.ReceivableByOffset(offset)
+		if err != nil {
+			return err
+		}
+	}
+
+	if offset > s.MaxDataLimit {
+		return qtype.FlowControlError
+	}
+	return nil
 }
 
 func (s *StreamFlowController) updateLargestReceived(offset uint64) {
 	s.largestReceived = offset
-	if !s.IsStreamZero {
-		s.connFC.updateByteReceived(offset) // TODO: not correct
-	}
 }
 
 func (s *StreamFlowController) updateLargestSent(offset uint64) {
 	s.largestSent = offset
-	if !s.IsStreamZero {
-		s.connFC.updateByteSent(offset) // TODO: not correct
-	}
 }
 
 type ConnectionFlowController struct {
 	baseFlowController
 }
 
-func (c *ConnectionFlowController) updateByteSent(bytes uint64) {
-	c.bytesSent += bytes
+func (c *ConnectionFlowController) SendableByOffset(largestOffset uint64) bool {
+	return c.bytesSent+largestOffset <= c.MaxDataLimit
 }
-func (c *ConnectionFlowController) updateByteReceived(bytes uint64) {
-	c.bytesReceived += bytes
+
+func (c *ConnectionFlowController) ReceivableByOffset(largestOffset uint64) error {
+	if c.bytesReceived+largestOffset > c.MaxDataLimit {
+		return qtype.FlowControlReceivedTooMuchData
+	}
+	return nil
+}
+
+func (c *ConnectionFlowController) updateByteSent(largestOffset uint64) {
+	c.bytesSent += largestOffset
+}
+func (c *ConnectionFlowController) updateByteReceived(largestOffset uint64) {
+	c.bytesReceived += largestOffset
 }
