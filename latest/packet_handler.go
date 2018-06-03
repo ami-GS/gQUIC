78e7a7f8e9a061697f7a27f0f68815635e02e532
ami-GS <1991.daiki@gmail.com>
2018-06-03 15:53:32 +0900

    add packet hander interface

---
+++ latest/packet_handler.go
@@ -0,0 +1,30 @@
+package quiclatest
+
+type PacketHandler interface {
+	handleInitialPacket(p *InitialPacket) error
+	handleVersionNegotiationPacket(p *VersionNegotiationPacket) error
+	handleRetryPacket(p *RetryPacket) error
+	handleHandshakePacket(p *HandshakePacket) error
+}
+
+type BasePacketHandler struct{}
+
+func (h *BasePacketHandler) handleInitialPacket(p *InitialPacket) error {
+	// log not implemented
+	return nil
+}
+
+func (h *BasePacketHandler) handleVersionNegotiationPacket(p *VersionNegotiationPacket) error {
+	// log not implemented
+	return nil
+}
+
+func (h *BasePacketHandler) handleRetryPacket(p *RetryPacket) error {
+	// log not implemented
+	return nil
+}
+
+func (h *BasePacketHandler) handleHandshakePacket(p *HandshakePacket) error {
+	// log not implemented
+	return nil
+}
