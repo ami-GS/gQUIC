e438e153c05ffba17213fee48b58dc4b71eead76
ami-GS <1991.daiki@gmail.com>
2018-06-05 22:25:47 +0900

    add more utility methods

--- latest/qtype/packet_number.go
+++ latest/qtype/packet_number.go
@@ -1,6 +1,7 @@
 package qtype
 
 import (
+	"encoding/binary"
 	"math/rand"
 )
 
@@ -23,6 +24,23 @@ func (pn *PacketNumber) Increase() PacketNumber {
 	return *pn
 }
 
+func (pn PacketNumber) Bytes() []byte {
+	var wire []byte
+	switch {
+	case pn <= 255:
+		wire = make([]byte, 1)
+		wire[0] = byte(pn)
+	case pn <= (1<<16)-1:
+		wire = make([]byte, 2)
+		binary.BigEndian.PutUint16(wire, uint16(pn))
+	case pn <= (1<<32)-1:
+		wire = make([]byte, 4)
+		binary.BigEndian.PutUint32(wire, uint32(pn))
+	}
+
+	return wire
+}
+
 func (pn PacketNumber) Size() int {
 	switch {
 	case pn <= 255:
@@ -31,6 +49,18 @@ func (pn PacketNumber) Size() int {
 		return 2
 	case pn <= (1<<32)-1:
 		return 4
+	}
+	panic("")
+}
+
+func (pn PacketNumber) Flag() byte {
+	switch {
+	case pn <= 255:
+		return 0x00
+	case pn <= (1<<16)-1:
+		return 0x01
+	case pn <= (1<<32)-1:
+		return 0x02
 	}
 	panic("")
 }
