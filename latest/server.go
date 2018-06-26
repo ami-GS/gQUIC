749c1df0261e5147a43b99f6a26d5e8e805563f6
ami-GS <1991.daiki@gmail.com>
2018-06-26 15:53:22 +0900

    add mutex for session RW

--- latest/server.go
+++ latest/server.go
@@ -12,6 +12,7 @@ type Server struct {
 	// TODO: replace to SessionStore
 	sessions          map[string]*Session //ConnectionID.String():Session
 	addrSessions      map[string]*Session //remoteAddr.String():Session, for identifing single connection if zero-len dest ID
+	sessionsMutex     *sync.Mutex
 	SupportedVersions []qtype.Version
 	// TODO: consider here? or in utility? or decide dynamically?
 	SessionLimitNum int
@@ -32,6 +33,7 @@ func ListenAddr(addr string) (*Server, error) {
 		conn:              conn,
 		sessions:          make(map[string]*Session),
 		addrSessions:      make(map[string]*Session),
+		sessionsMutex:     new(sync.Mutex),
 		SupportedVersions: qtype.SupportedVersions,
 		// TODO: experimental
 		SessionLimitNum: 1000,
@@ -94,7 +96,9 @@ func (s *Server) handlePacket(remoteAddr net.Addr, packet Packet) error {
 
 	var sess *Session
 	if len(destID) != 0 {
+		s.sessionsMutex.Lock()
 		sess, ok = s.sessions[destID.String()]
+		s.sessionsMutex.Unlock()
 		if !ok {
 			if !s.IsAcceptableSession(lh.Version, srcID, destID, remoteAddr) {
 				return nil
@@ -102,6 +106,7 @@ func (s *Server) handlePacket(remoteAddr net.Addr, packet Packet) error {
 
 			// TODO: have to reset Session when Retry Packet sent to client. then thsi can use DestID for packet maching
 			sess = NewSession(&Connection{conn: s.conn, remoteAddr: remoteAddr}, destID, srcID, false)
+			sess.server = s
 			// packet handler for each session on server is now defined in session.go
 			sess.packetHandler = sess
 			// TODO: be careful to use lh
@@ -109,7 +114,9 @@ func (s *Server) handlePacket(remoteAddr net.Addr, packet Packet) error {
 			go sess.Run()
 
 			// might be deleted after handling packet
+			s.sessionsMutex.Lock()
 			s.sessions[destID.String()] = sess
+			s.sessionsMutex.Unlock()
 			s.addrSessions[remoteAddr.String()] = sess
 		}
 	} else {
@@ -133,7 +140,10 @@ func (s *Server) IsVersionSupported(version qtype.Version) bool {
 }
 
 func (s *Server) IsAcceptableSession(version qtype.Version, srcID, destID qtype.ConnectionID, remoteAddr net.Addr) bool {
-	if len(s.sessions) >= s.SessionLimitNum {
+	s.sessionsMutex.Lock()
+	sessionNum := len(s.sessions)
+	s.sessionsMutex.Unlock()
+	if sessionNum >= s.SessionLimitNum {
 		p := NewHandshakePacket(version, srcID, destID, qtype.InitialPacketNumber(),
 			[]Frame{NewConnectionCloseFrame(qtype.ServerBusy, "The number of session reached server limit")})
 		wire, err := p.GetWire()
@@ -144,6 +154,20 @@ func (s *Server) IsAcceptableSession(version qtype.Version, srcID, destID qtype.
 		return false
 	}
 	return true
+}
+
+func (s *Server) DeleteSessionFromMap(ID qtype.ConnectionID) {
+	s.sessionsMutex.Lock()
+	delete(s.sessions, ID.String())
+	s.sessionsMutex.Unlock()
+}
+
+func (s *Server) ChangeConnectionID(fromID, toID qtype.ConnectionID) {
+	s.sessionsMutex.Lock()
+	session := s.sessions[fromID.String()]
+	delete(s.sessions, fromID.String())
+	s.sessions[toID.String()] = session
+	s.sessionsMutex.Unlock()
 }
 
 func (s *Server) SendVersionNegotiationPacket(srcID, destID qtype.ConnectionID, remoteAddr net.Addr) error {
