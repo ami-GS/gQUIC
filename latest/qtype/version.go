ecbb418e7b16bedbd86b2620d2658ec35abcd807
ami-GS <1991.daiki@gmail.com>
2018-06-27 14:45:45 +0900

    implement detail and add version for unsupported version test

--- latest/qtype/version.go
+++ latest/qtype/version.go
@@ -6,9 +6,19 @@ type Version uint32
 
 //TODO: fill appropriate versions
 const (
-	VersionPlaceholder Version = math.MaxUint32
-	VersionQuicTLS     Version = 0x00000001
-	VersionZero        Version = 0 // for test use
+	VersionPlaceholder     Version = math.MaxUint32
+	VersionQuicTLS         Version = 0x00000001
+	VersionUnsupportedTest Version = 0x12345678
+	VersionZero            Version = 0 // for test use
 )
 
-var SupportedVersions = []Version{VersionZero}
+var SupportedVersions = []Version{VersionQuicTLS, VersionPlaceholder}
+
+func (v Version) String() string {
+	return map[Version]string{
+		VersionPlaceholder:     "Placeholder",
+		VersionQuicTLS:         "QuicTLS",
+		VersionUnsupportedTest: "UnsupportedTest",
+		VersionZero:            "Zero",
+	}[v]
+}
