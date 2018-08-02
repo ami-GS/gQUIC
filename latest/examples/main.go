a9461b0623b3a92fa5a1c4f99994c06268f01b38
ami-GS <1991.daiki@gmail.com>
2018-08-01 20:32:15 -0700

    update example to include almost feature

--- latest/examples/main.go
+++ latest/examples/main.go
@@ -11,23 +11,28 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
 	cli, err := quiclatest.DialAddr("127.0.0.1:8080")
 	if err != nil {
 		panic(err)
 	}
-	dataSize := 2000
-	data := make([]byte, dataSize)
-	for i := 0; i < dataSize; i++ {
-		data[i] = byte(i%126) + 49
-	}
 
+	dataSize := 2000
+	basedata := make([]byte, dataSize)
+	for i := 0; i < dataSize; i++ {
+		basedata[i] = byte(i%126) + 49
+	}
 	time.Sleep(500 * time.Millisecond)
 	for i := 0; i < 3; i++ {
-		cli.Send(data)
+		basedata = append(basedata, '@')
+		go cli.Send(basedata)
 	}
 
 	time.Sleep(1 * time.Second)
 
+	cli.Ping()
+	time.Sleep(500 * time.Millisecond)
+
 	cli.Close()
-	time.Sleep(4 * time.Second)
+	time.Sleep(2 * time.Second)
 }
